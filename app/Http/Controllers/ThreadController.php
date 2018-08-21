868545eb62563dd1c85c3bb785804425ea41b65e
Antonius Yonathan <atomicbomber.email@gmail.com>
2018-08-21 16:23:59 +0700

    Fixed the comments ordering problem.

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Thread;
+use Jenssegers\Date\Date;
 use DB;
 
 class ThreadController extends Controller
@@ -32,24 +33,30 @@ class ThreadController extends Controller
 
     public function detail(Thread $thread)
     {
+        DB::statement('SET @row_number = 0');
+        $sub_query = DB::table('comments')
+            ->select('comments.id', DB::raw('@row_number:=@row_number+1 AS row_number'))
+            ->whereRaw('thread_id', $thread->id)
+            ->orderBy('created_at');
+
         $comment_tree = DB::table('comment_paths')
-            ->select(DB::raw('GROUP_CONCAT(ancestor_id ORDER BY ancestor_id) AS hierarchy'), 'users.name AS poster_name', 'comments.content')
+            ->select(
+                'comments.id',
+                DB::raw('GROUP_CONCAT(x.row_number ORDER BY tree_depth DESC) AS hierarchy'),
+                DB::raw('COUNT(*) AS tree_depth'),
+                'users.name AS poster_name',
+                'comments.content', 'comments.created_at')
             ->join('comments', 'comments.id', '=', 'comment_paths.descendant_id')
+            ->joinSub($sub_query, 'x', function($join) {
+                $join->on('x.id', '=', 'comment_paths.ancestor_id');
+            })
             ->join('users', 'users.id', '=', 'comments.poster_id')
-            ->groupBy('descendant_id', 'poster_name', 'comments.content')
+            ->where('comments.thread_id', $thread->id)
+            ->groupBy('comments.id', 'poster_name', 'comments.content', 'comments.created_at')
             ->orderBy('hierarchy')
-            ->orderBy('comments.created_at')
-            ->get()
-            ->map(
-                function ($item) {
-                    $temp = explode(',', $item->hierarchy);
-                    return [
-                        'id' => (int) end($temp),
-                        'tree_depth' => count($temp),
-                        'poster_name' => $item->poster_name,
-                        'content' => $item->content
-                    ];
-                });
+            ->get();
+
+        // return $comment_tree;
                 
         return view('thread.detail', [
             'thread' => $thread,
